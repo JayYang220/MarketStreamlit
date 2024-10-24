568f8331654143e4ea139cc3ae4b0719b0a7b3fd
JayYang220 <y123154@gmail.com>
2024-10-25 01:05:37 +0800

    1.1.3

--- Main/API/stockInfo.py
+++ Main/API/stockInfo.py
@@ -4,17 +4,11 @@ import yfinance as yf
 import os
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, MultiHeadAttention, LayerNormalization, Dropout
-from tensorflow.keras.models import Model, Sequential
 import streamlit as st
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-# import matplotlib.pyplot as plt
 from common import debug_msg
 
-is_debug = True
+is_debug = False
 
 class StockManager:
     __slots__ = ["history_data_folder_path", "stock_name_list", "stock_class_list", "msg", "is_action_successful"]
@@ -236,15 +230,6 @@ class Stock:
                 return f"{self.stock_name}: Stock name error. You can retrieve stock names from Yahoo Finance. https://finance.yahoo.com/."
         except Exception as e:
             return f"{self.stock_name}: {e}"
-
-    def test(self):
-        a = TestClass(self.history_data_file_path, self.stock_name)
-        msg = st.empty()
-        msg.write(f"###### Please wait a moment. This may take a few minutes.")
-        a.step1()
-        result = a.step2()
-        msg.write(f"###### Done.")
-        return result
     
     def show_data(self):
         self.data = pd.read_csv(self.history_data_file_path)
@@ -265,119 +250,3 @@ class Stock:
             # 使用st.plotly_chart顯示圖表
             st.plotly_chart(fig)
 
-
-class TestClass():
-    def __init__(self, history_data_file_path, stock_name):
-        self.scaler = MinMaxScaler(feature_range=(0, 1))
-        self.time_step = 60  # 使用前60天的數據進行預測
-        self.scaled_close_prices = None
-        self.x_train = None
-        self.x_test = None
-        self.y_train = None
-        self.y_test = None
-
-        self.stock_name = stock_name
-        self.data = pd.read_csv(history_data_file_path)
-
-    def step1(self):
-        # 選擇'Close'
-        close_prices = self.data[['Close']].values
-
-        # 標準化數據
-        self.scaled_close_prices = self.scaler.fit_transform(close_prices)
-
-        # 創建訓練和測試數據集
-        x, y = self.create_dataset(self.scaled_close_prices, self.time_step)
-
-        # 拆分數據集為訓練和測試集
-        train_size = int(len(x) * 0.8)
-        test_size = len(x) - train_size
-        self.x_train, self.x_test = x[0:train_size], x[test_size:len(x)]
-        self.y_train, self.y_test = y[0:train_size], y[test_size:len(y)]
-
-        # 將數據reshape為Transformer模型的輸入格式
-        self.x_train = self.x_train.reshape(self.x_train.shape[0], self.x_train.shape[1], 1)
-        self.x_test = self.x_test.reshape(self.x_test.shape[0], self.x_test.shape[1], 1)
-
-    def step2(self):
-        # 模型參數
-        input_shape = (self.time_step, 1)  # 使用前60天的數據進行預測
-        embed_dim = 64
-        num_heads = 2
-        ff_dim = 32
-
-        # 構建模型
-        transformer_model = self.build_transformer_model(input_shape, embed_dim, num_heads, ff_dim)
-        # 編譯模型
-        transformer_model.compile(optimizer='adam', loss='mean_squared_error')
-        # 訓練模型
-        transformer_model.fit(self.x_train, self.y_train, batch_size=32, epochs=50, validation_data=(self.x_test, self.y_test))
-
-        # 使用模型進行預測
-        train_predict = transformer_model.predict(self.x_train)
-        test_predict = transformer_model.predict(self.x_test)
-
-        # 反標準化數據
-        train_predict = self.scaler.inverse_transform(train_predict)
-        test_predict = self.scaler.inverse_transform(test_predict)
-        self.y_train = self.scaler.inverse_transform([self.y_train])
-        self.y_test = self.scaler.inverse_transform([self.y_test])
-
-        # 使用Streamlit顯示結果
-        st.subheader('Stock Price Prediction using Transformer')
-        st.subheader(f'{self.stock_name} Stock Price')
-
-        # 繪製結果
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=np.arange(len(self.scaled_close_prices)), y=self.scaler.inverse_transform(self.scaled_close_prices).flatten(), mode='lines', name='原始數據'))
-        fig.add_trace(go.Scatter(x=np.arange(self.time_step, len(train_predict) + self.time_step), y=train_predict.flatten(), mode='lines', name='Training Prediction'))
-        fig.add_trace(go.Scatter(x=np.arange(len(train_predict) + (2 * self.time_step), len(train_predict) + (2 * self.time_step) + len(test_predict)), y=test_predict.flatten(), mode='lines', name='Testing Prediction'))
-        
-        # Set chart layout
-        fig.update_layout(
-            title=f"{self.stock_name}",
-            xaxis_title="Date",
-            yaxis_title="Closing Price (USD)",
-            xaxis_rangeslider_visible=True
-        )
-        return fig
-
-    @staticmethod
-    def create_dataset(data, time_step=1):
-        X, y = [], []
-        for i in range(len(data) - time_step - 1):
-            a = data[i:(i + time_step), 0]
-            X.append(a)
-            y.append(data[i + time_step, 0])
-        return np.array(X), np.array(y)
-
-    # 構建Transformer模型
-    def build_transformer_model(self, input_shape, embed_dim, num_heads, ff_dim):
-        inputs = tf.keras.Input(shape=input_shape)
-        transformer_block = self.TransformerEncoder(embed_dim, num_heads, ff_dim)
-        x = transformer_block(inputs)
-        x = Flatten()(x)
-        x = Dense(20, activation="relu")(x)
-        x = Dropout(0.1)(x)
-        outputs = Dense(1)(x)
-        model = Model(inputs=inputs, outputs=outputs)
-        return model
-
-    # 定義Transformer Encoder層
-    class TransformerEncoder(tf.keras.layers.Layer):
-        def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
-            super(TestClass.TransformerEncoder, self).__init__()
-            self.att = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
-            self.ffn = Sequential([Dense(ff_dim, activation="relu"), Dense(embed_dim)])
-            self.layernorm1 = LayerNormalization(epsilon=1e-6)
-            self.layernorm2 = LayerNormalization(epsilon=1e-6)
-            self.dropout1 = Dropout(rate)
-            self.dropout2 = Dropout(rate)
-
-        def call(self, inputs, training):
-            attn_output = self.att(inputs, inputs)
-            attn_output = self.dropout1(attn_output, training=training)
-            out1 = self.layernorm1(inputs + attn_output)
-            ffn_output = self.ffn(out1)
-            ffn_output = self.dropout2(ffn_output, training=training)
-            return self.layernorm2(out1 + ffn_output)
