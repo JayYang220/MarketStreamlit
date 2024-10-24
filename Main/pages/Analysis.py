cba729a37b2e804cfec20def55236d1c9d0a691b
JayYang220 <y123154@gmail.com>
2024-10-25 00:43:50 +0800

    1.1.1

--- Main/pages/Analysis.py
+++ Main/pages/Analysis.py
@@ -9,15 +9,9 @@ if stock_list:
     st.subheader("Analysis:")
     with st.form(key='Analysis'):
         form_gender = st.selectbox('Please select a item:', manager.get_stock_list())
-
-        submit_button = st.form_submit_button(label='Start')
-        submit_button2 = st.form_submit_button(label='Show Data')
+        submit_button = st.form_submit_button(label='Show Data')
 
         if submit_button:
-            fig = manager.get_analysis(form_gender)
-            st.plotly_chart(fig)
-
-        if submit_button2:
             manager.show_data(form_gender)
 
 else:
